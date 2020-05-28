5d5f6c2ba43fcaa2567bdda4a144ccd0ab4e39a7
Achintya <achin1tya@gmail.com>
2020-05-28 12:23:11 +0530

    added build file

---
+++ client/build/precache-manifest.d8c5b82a0a1e27f775ac198077ac6627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29daa1c45e06da0c6b0b0c084cf32c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fdd01b246dc0aaf0fd9",
+    "url": "/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "17501b575395334533a7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2fdd01b246dc0aaf0fd9",
+    "url": "/static/js/2.6edea1ea.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.6edea1ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17501b575395334533a7",
+    "url": "/static/js/main.a2d222e1.chunk.js"
+  },
+  {
+    "revision": "aee12e94df29fdf2e9cf",
+    "url": "/static/js/runtime-main.6e64ad1a.js"
+  }
+]);
