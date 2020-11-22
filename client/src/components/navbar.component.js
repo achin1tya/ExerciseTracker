c973587a6dff9ee4de5444baac31d5c814ae93e9
Achintya <achin1tya@gmail.com>
2020-11-22 14:11:03 +0530

    fix for issue #1 Navbar not visible in small screen devices

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg" >
+            <nav className="navbar navbar-dark bg-dark navbar-expand" >
             <Link to="/" className="navbar-brand">Exercise Tracker</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto" >
